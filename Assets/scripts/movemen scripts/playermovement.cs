a674ae5dd862192db6c22d72da5d80a9626d8482
Ashisaxena1826 <ashi99076@gmail.com>
2024-08-21 23:25:07 +0530

    player movement

---
+++ Assets/scripts/movemen scripts/playermovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 movementInput;
+    public float movementSpeed = 0.1f;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 20f;
+    public float verticalSpeed = 0;
+    public void IAMovement(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void IAJump(InputAction.CallbackContext context)
+    {
+        if (context.started == true)
+        {
+            verticalSpeed = jumpSpeed;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GroundCheck() == true)
+        {
+            verticalSpeed = 0;
+        }
+        else
+        {
+            verticalSpeed = (verticalSpeed - gravity) * Time.deltaTime;
+        }
+        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, verticalSpeed, movementInput.y * movementSpeed * Time.deltaTime);
+    }
+
+    public bool GroundCheck()
+    {
+        return Physics.Raycast(transform.position, transform.up * -1, 1);
+    }
+}
